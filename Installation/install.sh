cf58c9a816d507f4bd51bccdd7cdf70f1996fc4f
Naresh Thukkani <naresh@criterionnetworks.com>
2017-04-02 19:59:55 +0530

    Installation procedure

---
+++ Installation/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Update to latest release
+#
+echo "Update installed software to latest release"
+sudo apt-get update
+#
+# Install missing packages
+#
+echo "Install missing packages"
+sudo apt-get install python-setuptools python-pip git ack-grep jq
+#
+# Install Python components
+#
+echo "Install required Python components"
+sudo apt-get install python-yaml python-httplib2 python-pysnmp4
+sudo pip install jinja2 six bracket-expansion netaddr
+#
+echo "Install optional Python components"
+sudo pip install yamllint tree
+#
+# Install latest stable Ansible version from Ansible reposityr
+#
+sudo apt-get install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
